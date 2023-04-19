18a48547a7f5c20b3110522e2ee70483cd040efb
Ilia Belikov <gumuman@mail.ru>
2023-04-19 19:37:48 +0600

    added text

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         new Application().run();
+
+        System.out.println("Hello".repeat(150));
+        System.out.println();
+        System.out.println("World".repeat(150));
     }
 }
